b748d5ff5ead6e251216c78e95bbdaec2eacbe7c
David Grabosky <david.grabosky0401@gmail.com>
2023-05-16 20:29:52 -0700

    Added color randomization via button and page load

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 // Declaring variables
 var body = document.getElementById("gradient");
+var button = document.getElementById("randomize");
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
+
+function randomizeColors() {
+    color1.value = '#'+(Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0');
+    color2.value = '#'+(Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0');
+}
 
 // Function to set new gradient values
 function setGradient() {
@@ -19,4 +25,9 @@ color1.addEventListener("input", setGradient);
 color2.addEventListener("input", setGradient);
 
 // Display initial value of gradient
-window.addEventListener("load", setGradient)
+window.addEventListener("load", randomizeColors)
+window.addEventListener("load", setGradient);
+
+// Randomizing the gradient via button
+button.addEventListener("click", randomizeColors)
+button.addEventListener("click", setGradient)
