41f276fc83bfcfab199562cf4f58a1ea7cfcfedb
David Grabosky <david.grabosky0401@gmail.com>
2023-05-16 19:50:51 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// Declaring variables
+var body = document.getElementById("gradient");
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+
+// Function to set new gradient values
+function setGradient() {
+  body.style.background =
+    "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+}
+
+// Adjusting gradient based on color1 value
+color1.addEventListener("input", setGradient);
+
+// Adjusting gradient based on color2 value
+color2.addEventListener("input", setGradient);
